a425a89d4f0a14f20384960936d484a4b6cf61cf
eunsang.kim <gomuzom@naver.com>
2022-10-04 00:32:03 +0900

    feat: viewmodel, screen 연동, debounce적용

--- lib/presentation/company_listings/company_listings_view_model.dart
+++ lib/presentation/company_listings/company_listings_view_model.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:stock_app/domain/repository/stock_repository.dart';
+import 'package:stock_app/presentation/company_listings/company_listings_action.dart';
 import 'package:stock_app/presentation/company_listings/company_listings_state.dart';
 
 class CompanyListingsViewModel with ChangeNotifier {
@@ -8,6 +11,20 @@ class CompanyListingsViewModel with ChangeNotifier {
   CompanyListingsState _state = CompanyListingsState();
 
   CompanyListingsState get state => _state;
+
+  Timer? _debounce;
+
+  void onAction(CompanyListingsAction action) {
+    action.when(
+      refresh: () => _getCompanyListings(fetchFromRemote: true),
+      onSearchQueryChange: (query) {
+        _debounce?.cancel();
+        _debounce = Timer(Duration(milliseconds: 700), () {
+          _getCompanyListings(query: query);
+        });
+      },
+    );
+  }
 
   CompanyListingsViewModel(this._repository) {
     _getCompanyListings();
